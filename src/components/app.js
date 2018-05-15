84838d971635491d9432fe3f33fd0474592601e7
Malte Modrow <modrow@ubilabs.net>
2018-05-15 23:15:32 +0200

    feat(setup): add styled components

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 class App extends Component {
   render() {
     return (
-      <p>
+      <Title>
         To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+      </Title>
     );
   }
 }
