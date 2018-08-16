87c26496c348ad799a88b3b5d282da8b2d7ff43f
Malte Modrow <modrow@ubilabs.net>
2018-08-16 17:13:19 +0200

    add new genre

--- src/constants/genres.js
+++ src/constants/genres.js
@@ -4,6 +4,7 @@ export default [
   {name: 'rap', color: '#ff8a65'},
   {name: 'ambient', color: '#ffd54f'},
   {name: 'punk', color: '#81c784'},
+  {name: 'elektropunk', color: '#00b0ff'},
   {name: 'house', color: '#4dd0e1'},
   {name: 'rock', color: '#f06292'},
   {name: 'trance', color: '#81c784'}
