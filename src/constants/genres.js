e4389213045af432f36fdb84113d0e43fe9f4a2a
Malte Modrow <modrow@ubilabs.net>
2020-01-20 11:21:04 +0100

    add post punk

--- src/constants/genres.js
+++ src/constants/genres.js
@@ -5,6 +5,7 @@ export default [
   {name: 'rap', color: '#ff8a65'},
   {name: 'ambient', color: '#ffd54f'},
   {name: 'punk', color: '#81c784'},
+  {name: 'post-punk', color: '#ff5722'},
   {name: 'elektropunk', color: '#00b0ff'},
   {name: 'house', color: '#4dd0e1'},
   {name: 'rock', color: '#f06292'},
