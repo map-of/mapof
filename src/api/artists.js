ce8cf5ec65a7446b1dcb47839da7557b1420f9bc
Malte Modrow <modrow@ubilabs.net>
2020-12-01 11:07:10 +0100

    fix maybe?

--- src/api/artists.js
+++ src/api/artists.js
@@ -1,26 +1,25 @@
 export function fetchArtists(genre) {
   return fetch(
-    'https://script.google.com/macros/s/' +
-      'AKfycbytro_BuOciH12QClPlDg1GF60DdHCsMgN3MZGqaq6QfhUvfwkB/exec?' +
+    "https://script.google.com/macros/s/" +
+      "AKfycbytro_BuOciH12QClPlDg1GF60DdHCsMgN3MZGqaq6QfhUvfwkB/exec?" +
       `genre=${genre}`,
     {
-      method: 'GET'
+      method: "GET",
     }
-  ).then(
-    response => (response.ok ? response.text() : Promise.reject(response))
+  ).then((response) =>
+    response.ok ? response.json() : Promise.reject(response)
   );
 }
 
 export function submitArtist(artist) {
   return fetch(
-    'https://script.google.com/macros/s/' +
-      'AKfycbytro_BuOciH12QClPlDg1GF60DdHCsMgN3MZGqaq6QfhUvfwkB/exec',
+    "https://script.google.com/macros/s/" +
+      "AKfycbytro_BuOciH12QClPlDg1GF60DdHCsMgN3MZGqaq6QfhUvfwkB/exec",
     {
-      method: 'POST',
-      body: JSON.stringify(artist)
+      method: "POST",
+      body: JSON.stringify(artist),
     }
-  ).then(
-    response =>
-      response.ok ? Promise.resolve('Success') : Promise.reject(response)
+  ).then((response) =>
+    response.ok ? Promise.resolve("Success") : Promise.reject(response)
   );
 }
